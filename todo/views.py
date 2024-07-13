94659e527d47fcb45e97533585b5c9ccca1ebfa2
iniad <s1f102301190@iniad.org>
2024-07-13 12:37:21 +0900

    更新機能

--- todo/views.py
+++ todo/views.py
@@ -33,6 +33,7 @@ def detail(request, task_id):
     }
     return render(request, 'todo/detail.html', context)
 
+
 def update(request, task_id):
     try:
         task = Task.objects.get(pk=task_id)
